a0627826d093b3598ee38b11c7d6b00041577f64
Mason Edwards <edwardsmason2000@gmail.com>
2022-10-14 04:16:26 +0100

    Added initial project files

---
+++ ComputingProjectTest/DataGeneratingDevice/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using System.Net.Http.Headers;
+
+Console.Write("------DataGeneratingProgram------\n\n");
+
+// Write to kafka topic.
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    for (var i = 0; i < 100; i++)
+    {
+        // Generate some random data
+        String data = Convert.ToString(new Random().Next(1, 100));
+
+        var result = await producer.ProduceAsync("TelemetryData", new Message<Null, string> { Value = $"{data}", Timestamp = new Timestamp(DateTimeOffset.Now) });
+        Console.WriteLine($"{result.Message.Timestamp.UtcDateTime} : {result.Message.Value}");
+        Console.Out.Flush();
+
+        await Task.Delay(5000);
+    }
+}
+
+// Simple console app to simulate a system generating telemetry data
---
+++ ComputingProjectTest/DataReadingProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+Console.Write("------DataReadingProgram------\n\n");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "TelemetryReader",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe(new List<string> { "TelemetryData" });
+
+
+    // Consumer Loop
+    while(true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"{consumeResult.Message.Timestamp.UtcDateTime} : {consumeResult.Message.Value}");
+        Console.Out.Flush();
+
+        Task.Delay(5000);
+    }
+}
